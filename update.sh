fb84e37b0e5ff97803cd3e288ee288f34d094b7f
yzkaa <1076992525@qq.com>
2021-01-10 15:00:56 +0800

    2021.01.10

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 #git update
 is_date=`date "+%Y.%m.%d"`
+cd ~/C++
 git add .
 git commit -m "${is_date}"
 git push cpp master
+
+cd ~/Nutstore Files/
+git add.
+git commit "${is_date}"
+git push note master
+
